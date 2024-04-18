0524c387ce35bb714db8ffd5f8196f9014511e4c
Safonov <Sofik.d@gmail.com>
2024-04-18 21:50:43 +0300

    Обхявление моделей

--- app/admin/models.py
+++ app/admin/models.py
@@ -1,5 +1,14 @@
+from typing import Optional
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
 from app.store.database.sqlalchemy_base import BaseModel
 
 
 class AdminModel(BaseModel):
     __tablename__ = "admins"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String[30], unique=True, index=True)
+    password: Mapped[Optional[str]] = mapped_column(String(128))
--- app/quiz/models.py
+++ app/quiz/models.py
@@ -1,13 +1,39 @@
+from typing import List
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import Boolean
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
 from app.store.database.sqlalchemy_base import BaseModel
 
 
 class ThemeModel(BaseModel):
     __tablename__ = "themes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String[300], unique=True, index=True)
+
+    questions: Mapped[List["QuestionModel"]] = relationship(back_populates="theme", cascade="all, delete-orphan")
 
 
 class QuestionModel(BaseModel):
     __tablename__ = "questions"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String[300], unique=True)
+
+    theme_id: Mapped[int] = mapped_column(ForeignKey("themes.id"))
+    theme: Mapped["ThemeModel"] = relationship(back_populates="questions")
+
+    answers: Mapped[List["AnswerModel"]] = relationship(back_populates="question", cascade="all, delete-orphan")
 
 
 class AnswerModel(BaseModel):
     __tablename__ = "answers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String[300], unique=True)
+    is_correct: Mapped[bool] = mapped_column(Boolean)
+
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+    question: Mapped["QuestionModel"] = relationship(back_populates="answers")
+
